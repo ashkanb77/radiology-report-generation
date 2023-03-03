4d556e8f93a575499330d56e12a9b5f522b25290
ashkan <bagheriashkan77@gmail.com>
2023-03-03 22:49:55 +0330

    Add files via upload

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
-!pip install git+https://github.com/huggingface/transformers.git@main
+pip install git+https://github.com/huggingface/transformers.git@main
 
-%mkdir images
-!tar -xzvf "drive/MyDrive/NLMCXR_png.tgz" -C 'images'
+mkdir images
+tar -xzvf "drive/MyDrive/NLMCXR_png.tgz" -C 'images'
 
-!tar -xzvf "drive/MyDrive/NLMCXR_reports.tgz" -C 'reports'
+tar -xzvf "drive/MyDrive/NLMCXR_reports.tgz" -C 'reports'
